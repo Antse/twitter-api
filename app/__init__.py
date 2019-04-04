d486fbf24708ea0e07b757cce79f9f8ed3410e65
antoine <antoine.azlab@outlook.com>
2019-04-04 16:21:51 +0200

    twitter API day 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Api
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
 
     @app.route('/hello')
     def hello():
@@ -15,3 +23,18 @@ def create_app():
 
     app.config['ERROR_404_HELP'] = False
     return app
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     @app.route('/hello')
+#     def hello():
+#         return "Goodbye World!"
+
+#     from .apis.tweets import api as tweets
+#     api = Api()
+#     api.add_namespace(tweets)
+#     api.init_app(app)
+
+#     app.config['ERROR_404_HELP'] = False
+#     return app
